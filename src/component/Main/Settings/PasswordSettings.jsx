84bfdf9e67f8c0e3116e8f77461b765d8489036a
Sakib Ahmad <sakibahmad@Quantums-Mac-mini.local>
2025-05-29 08:14:57 +0600

    updated code

--- src/component/Main/Settings/PasswordSettings.jsx
+++ src/component/Main/Settings/PasswordSettings.jsx
@@ -9,7 +9,7 @@ import { useChangePasswordMutation } from "../../../redux/features/profile/profi
 import { ImSpinner6 } from "react-icons/im";
 
 const PasswordSettings = () => {
-    const { data, refetch, isFetching } = useGetUserQuery();
+  const { data, refetch, isFetching } = useGetUserQuery();
   const user = data?.attributes?.user;
   const [form] = Form.useForm();
   const [changePassWithOldPass, { isLoading }] = useChangePasswordMutation();
@@ -31,16 +31,26 @@ const PasswordSettings = () => {
 
   return (
     <div className="h-[718px] p-10 bg-white inline-flex justify-start items-start gap-2.5">
-    <div className="w-[507px] inline-flex flex-col justify-start items-start gap-8">
+      <div className="w-[507px] inline-flex flex-col justify-start items-start gap-8">
         <div className="w-28 flex flex-col justify-start items-start gap-5">
-            <div className="self-stretch justify-start text-neutral-400 text-base font-normal font-['Kantumruy_Pro']">Profile Picture</div>
-            <img className="self-stretch h-28 rounded-full" src={`${imageBaseUrl}${user?.image}`} />
+          <div className="self-stretch justify-start text-neutral-400 text-base font-normal font-['Kantumruy_Pro']">
+            Profile Picture
+          </div>
+          <img
+            className="self-stretch h-28 rounded-full"
+            src={`${imageBaseUrl}${user?.image}`}
+          />
         </div>
-        <div data-property-1="Default" className="self-stretch h-14 p-2.5 bg-violet-400 rounded-lg shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)] outline outline-1 outline-offset-[-1px] outline-slate-500 inline-flex justify-center items-center gap-2.5">
-            <div className="text-center justify-start text-white text-base font-bold font-['Mulish']">Change Password</div>
+        <div
+          data-property-1="Default"
+          className="self-stretch h-14 p-2.5 bg-violet-400 rounded-lg shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)] outline outline-1 outline-offset-[-1px] outline-slate-500 inline-flex justify-center items-center gap-2.5"
+        >
+          <div className="text-center justify-start text-white text-base font-bold font-['Mulish']">
+            Change Password
+          </div>
         </div>
+      </div>
     </div>
-</div>
   );
 };
 
